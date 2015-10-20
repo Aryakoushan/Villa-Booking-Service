29dfcfae8d1c142fa6421cfa40a2a625b2d801a3
Ali Kabiri <alikabiri.perco@gmail.com>
2015-10-20 12:12:50 +0330

    webservice problem has been solved, "getAllVillas" method works fine using a buffer .

--- main.go
+++ main.go
@@ -3,16 +3,17 @@ package main
 
 import (
 	"fmt"
+	"bytes"
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/auth"
 	"encoding/json"
-	"net/http"
+	//"net/http"
 )
 
 func main() {
 	fmt.Println("Villa Booking API starting ...")
 	//Init()	//creates the database and the tables .
-	dummyVillaData()	//creates some dummty villas for testing purposes .
+	//dummyVillaData()	//creates some dummty villas for testing purposes .
 	m := martini.Classic()
 	m.Use(auth.Basic("root", "secret"))
 	
@@ -25,9 +26,8 @@ func Index() string{
 	return "Welcome to Villa Booking API !"
 }
 
-func GetAllVillas() http.ResponseWriter {
-	var w http.ResponseWriter
-	json.NewEncoder(w).Encode(getVillas())
-	//var res string
-	return w
+func GetAllVillas() string {
+	buf := new(bytes.Buffer)
+	json.NewEncoder(buf).Encode(getVillas())
+	return buf.String()
 }
