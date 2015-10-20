0037e57855ca2a42589cdf57bb82a11dba32198b
Ali Kabiri <alikabiri.perco@gmail.com>
2015-10-20 15:49:13 +0330

    before resolving the database-model problem .

--- model.go
+++ model.go
@@ -14,54 +14,33 @@ type Model struct {
 
 type Villa struct {
 	gorm.Model
-	ID 			int
 	Owner 		VillaOwner
 	Name 		string `sql:"size:255"` //Default size for string .
 	Description string `sql:"size:255"`
 	Table		ReservationTable
 	Gallery		ImageGallery
-	
-	CreatedAt    time.Time
-    UpdatedAt    time.Time
-    DeletedAt    *time.Time
 }
 
 type ReservationTable struct {
 	gorm.Model
-	ID				int
 	Reservations	[]Reservation
-	
-	CreatedAt    time.Time
-    UpdatedAt    time.Time
-    DeletedAt    *time.Time
 }
 
 type ImageGallery struct {
 	gorm.Model
-	ID			int
 	PicUrls		[]string
-	
-	CreatedAt    time.Time
-    UpdatedAt    time.Time
-    DeletedAt    *time.Time
 }
 
 type VillaOwner struct {
 	gorm.Model
-	ID			int
 	Name		string	`sql:"size:255"`
 	Villas		[]Villa
 	Pic			string	`sql:"size:255"`
 	Note		string	`sql:"size:255"`
-	
-	CreatedAt    time.Time
-    UpdatedAt    time.Time
-    DeletedAt    *time.Time
 }
 
 type Day struct {
 	gorm.Model
-	ID			int
 	ColorStat	string
 	price		float64
 	Date		time.Time
@@ -70,14 +49,9 @@ type Day struct {
 
 type Reservation struct {
 	gorm.Model
-	ID			int
 	AgentID		int
 	CustomerID	int
 	Days		[]Day
 	TotalPrice	float64
 	PaymentStat	string
-	
-	CreatedAt    time.Time
-    UpdatedAt    time.Time
-    DeletedAt    *time.Time
 }
