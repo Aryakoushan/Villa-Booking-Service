3e02992182fab796d55206787c1b341f73495739
Hamed Soleimani <hamed1soleimani@gmail.com>
2015-10-23 08:28:14 +0330

    clean and recreate files

--- doc.go
+++
@@ -1,6 +0,0 @@
-// villa_service_3 project doc.go
-
-/*
-villa_service_3 document
-*/
-package main
--- main.go
+++ main.go
@@ -3,30 +3,9 @@ package main
 
 import (
 	"fmt"
-	"bytes"
-	"github.com/go-martini/martini"
-	//"github.com/martini-contrib/auth"
-	"encoding/json"
 )
 
+//main funtion of project
 func main() {
-	fmt.Println("Villa Booking API starting ...")
-	//Init()	//creates the database and the tables .
-	//dummyVillaData()	//creates some dummty villas for testing purposes .
-	m := martini.Classic()
-	//m.Use(auth.Basic("root", "secret"))
-	
-	m.Get("/", Index)
-	m.Get("/getAllVillas", GetAllVillas)
-	m.Run()
-}
-
-func Index() string{
-	return "Welcome to Villa Booking API !"
-}
-
-func GetAllVillas() string {
-	buf := new(bytes.Buffer)
-	json.NewEncoder(buf).Encode(getVillas())
-	return buf.String()
+	fmt.Println("Aryakoushan")
 }
